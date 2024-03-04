28f063babc363ad84f9306adc5fe1f4dc311942c
Charlie Jones <125832247+CharlieJones2@users.noreply.github.com>
2024-03-04 17:01:12 +0000

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,18 +11,18 @@ weight = st.number_input('Weight (kg)', min_value=1, max_value=200, value=100, s
 sex = st.selectbox('Sex', ['Male', 'Female'])
 diet = st.selectbox('Diet/Lifestyle', ['Omnivore', 'Pescatarian', 'Vegetarian', 'Vegan'])
 if diet != 'Vegan':
-    improve.append('Go Vegan')
+    improve.append('According to a meta anaylsis conducted by Oxford University, going vegan is the single biggest positive change an individual can make to reduce their environmental impact. See here for details: https://doi.org/10.1126/science.aaq0216')
 social_activity = st.selectbox('How Often per Month do you Engage in Social Activity', ['Never', 'Sometimes', 'Often'])
 if social_activity == 'Often':
-    improve.append('Socialise Less')
+    improve.append('It is possible that engaging in less social activity could reduce your environmental impact. Consider how you travel to events and how sustainable they are.')
 
 st.header('Travel')
 air_travel = st.selectbox('How Often do you Travel by Air each Month?', ['Never', 'Rarely', 'Frequently', 'Very Frequently'])
 if air_travel != 'Rarely':
-    improve.append('Travel by Air Less')
+    improve.append('Travelling by air is detrimental to the environment. Consider the necessity of air travel and look to reduce it where possible.')
 transport = st.selectbox('Which of These is your Primary Travel Method?', ['Private', 'Public', 'Walk/Bicycle'])
 if transport == 'Private':
-    improve.append('Consider Public Transport or Walking')
+    improve.append('Consider Public Transport or Walking where possible for a lower carbon footprint. Traveling by public transport allows the environmental impact to be spread across many people, while walking or cycling removes this impact altogether.')
     vehicle_type = st.selectbox('Vehicle Fuel Type:', ['Petrol', 'LPG', 'Diesel', 'Hybrid', 'Electric'])
 else:
     vehicle_type = None
@@ -34,13 +34,13 @@ else:
 st.header('Energy Usage')
 shower = st.selectbox('How Often do you Shower?', ['Less Frequently', 'Daily', 'Twice a Day', 'More Frequently'])
 if shower == 'Twice a Day' or shower == 'More Frequently':
-    improve.append('Consider Showering Less Often, Daily is Enough for Most People')
+    improve.append('Consider showering less often: daily is enough for most people.')
 heating = st.selectbox('Which of These is your Primary Heating Source?', ['Coal', 'Natural Gas', 'Wood', 'Electricity'])
 if heating != 'Electricity':
-    improve.append('Consider switching to electric heating.')
+    improve.append('Consider switching to electric heating for a much more energy efficient solution. This not only reduces your environmental impact but can save you a considerable amount of money on energy bills.')
 energy_efficiency = st.selectbox('Do you Consider the Energy Efficiency of your Devices?', ['No', 'Sometimes', 'Yes'])
 if energy_efficiency != 'Yes':
-    improve.append('Consider the energy efficiency of your devices more.')
+    improve.append('Consider the energy efficiency of your devices more. Like choosing a sustainable heating source, considering the energy efficiency of devices also reduces your environmental impact while saving you money. Look for devices rated EPC A or B.')
     
 st.header('Waste and Recycling')
 waste_size = st.selectbox('How Big is your Waste Bag?', ['Small', 'Medium', 'Large', 'Extra Large'])
@@ -64,14 +64,16 @@ if clothes > 3:
     improve.append('Try to cut down on the amount of new clothing you buy. Wear what you already have, and try to shop secondhand and sutainable clothes where new purchases are necessary.')
 cook = st.multiselect('Which of the Following do you Cook With? (Select all that apply)', ['Stove', 'Oven', 'Microwave', 'Grill', 'Air Fryer'])
 if 'Stove' or 'Oven' or 'Grill' in cook:
-    improve.append('Consider cooking more with an air fryer or microwave where possible.')
+    improve.append('Consider cooking more with an air fryer or microwave where possible. Cooking on a stove, or in the grill or oven are particularly energy intensive processes and can generate larger emissions.')
 
 if 'calculatePressed' not in st.session_state:
     st.session_state.calculatePressed = False
     
+
 calculate = st.button('Calculate my Emissions')
 calculatePressed = False
 if calculate:
+    st.session_state.calculate = True
     st.session_state.calculatePressed = True
     emissions = Calculate(height, weight, sex, diet, social_activity, air_travel, transport, vehicle_type, distance, shower, heating, energy_efficiency, waste_size, waste_count, recycling, screen_time, internet, grocery, clothes, cook)
     
